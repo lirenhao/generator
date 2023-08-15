264ed855618d566362f70237ed9d0b71cf0f3514
lirenhao <renhao.li@bjyada.com>
2023-08-15 10:09:56 +0800

    实现模版

--- mybatis-plus-generator/src/main/java/com/baomidou/mybatisplus/generator/code/CodeGenerator.java
+++ mybatis-plus-generator/src/main/java/com/baomidou/mybatisplus/generator/code/CodeGenerator.java
@@ -9,7 +9,8 @@ public class CodeGenerator {
         CodeConfig.generator("common",
             new String[]{
 //                "biz_basic",
-                "biz_open_count", "biz_use_duration", "biz_use_count", "biz_keep_day", "biz_huahua_community"
+                  "biz_user_page_num"
+//                "biz_open_count", "biz_use_duration", "biz_use_count", "biz_keep_day", "biz_huahua_community", "active_user"
 //                "biz_spm_user", "biz_page_user", "biz_area_user",
 //                "biz_os_version_device", "biz_oems_device", "biz_device_model_device", "biz_network_type_device", "biz_telco_device", "biz_app_version_device",
             }
